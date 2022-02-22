ef959f1362f99aa1eaa3564e048ddedb070f5bb7
RohanDasari <rohandasari2000@gmail.com>
2022-02-22 08:04:33 +0530

    First Commit

---
+++ loginform/js/main.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAV3SVokGcFr1e04vvjjbFU3cag8L1VqaA",
+    authDomain: "loginform-7acae.firebaseapp.com",
+    projectId: "loginform-7acae",
+    storageBucket: "loginform-7acae.appspot.com",
+    messagingSenderId: "244622916096",
+    appId: "1:244622916096:web:de0135a2f8c130929936e5",
+    measurementId: "G-HPL4VJ18PJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
